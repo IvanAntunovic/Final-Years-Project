811beaed644110613fb8a7478ac08ad55fe61fb4
Ivan Antunović <doyouevenavr@gmail.com>
2017-07-16 22:27:55 +0200

    Add files via upload

---
+++ src/Timer/Timer.cpp
@@ -0,0 +1,99 @@
+/*
+ * Timer.cpp
+ *
+ * Created: 5/23/2017 8:35:50 PM
+ *  Author: fairenough
+ */ 
+#include "Timer.h"
+
+volatile unsigned long timer0_overflow_count = 0;
+volatile unsigned long timer0_millis = 0;
+static unsigned char timer0_fract = 0;
+
+Timer* Timer::sInstance = NULL;
+
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+ISR(TIM0_OVF_vect)
+#else
+ISR(TIMER0_OVF_vect)
+#endif
+{
+	// copy these to local variables so they can be stored in registers
+	// (volatile variables must be read from memory on every access)
+	unsigned long m = timer0_millis;
+	unsigned char f = timer0_fract;
+
+	m += MILLIS_INC;
+	f += FRACT_INC;
+	if (f >= FRACT_MAX) {
+		f -= FRACT_MAX;
+		m += 1;
+	}
+
+	timer0_fract = f;
+	timer0_millis = m;
+	timer0_overflow_count++;
+}
+
+Timer::Timer()
+{
+	// set up timer with prescaler = 64
+	TCCR0B |= (1 << CS01) | (1 << CS00);
+	// initialize counter
+	TCNT0 = 0;
+	// enable overflow interrupt
+	TIMSK0 |= (1 << TOIE0);
+	// enable global interrupts
+	sei();
+}
+
+Timer* Timer::getInstance()
+{
+	if (Timer::sInstance == NULL)
+	{
+		Timer::sInstance = new Timer();
+	}
+	return sInstance;
+}
+
+unsigned long Timer::millis()
+{
+	unsigned long m;
+	uint8_t oldSREG = SREG;
+
+	// disable interrupts while we read timer0_millis or we might get an
+	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+	cli();
+	m = timer0_millis;
+	SREG = oldSREG;
+
+	return m;
+}
+
+unsigned long Timer::micros() 
+{
+	unsigned long m;
+	uint8_t oldSREG = SREG, t;
+	
+	cli();
+	m = timer0_overflow_count;
+	#if defined(TCNT0)
+	t = TCNT0;
+	#elif defined(TCNT0L)
+	t = TCNT0L;
+	#else
+	#error TIMER 0 not defined
+	#endif
+
+	#ifdef TIFR0
+	if ((TIFR0 & _BV(TOV0)) && (t < 255))
+	m++;
+	#else
+	if ((TIFR & _BV(TOV0)) && (t < 255))
+	m++;
+	#endif
+
+	SREG = oldSREG;
+	
+	return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+}
---
+++ src/Timer/Timer.h
@@ -0,0 +1,49 @@
+/*
+ * Timer.cpp
+ *
+ * Created: 5/22/2017 7:32:51 PM
+ *  Author: fairenough
+ */ 
+
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
+#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
+
+
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
+
+// the whole number of milliseconds per timer0 overflow
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
+
+// the fractional number of milliseconds per timer0 overflow. we shift right
+// by three to fit these numbers into a byte. (for the clock speeds we care
+// about - 8 and 16 MHz - this doesn't lose precision.)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
+
+class Timer
+{
+	private:
+		static Timer* sInstance;
+
+	public:
+		Timer();
+		unsigned long millis();
+		unsigned long micros();
+		static Timer* getInstance();
+};
+
+
+#endif
